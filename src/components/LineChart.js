9641cf56e768a62c1fd23cf52604c96db84e1cad
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2022-04-17 13:08:07 -0400

    Fetched readings on Profile page, altered LineChart component for props passability

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -2,44 +2,50 @@ import React from "react";
 import { Line } from "react-chartjs-2";
 import Chart from "chart.js/auto";
 
-const state = {
-  labels: [90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200],
-  datasets: [
-    {
-      label: "Mmol/l",
-      backgroundColor: "rgba(237, 155, 40)",
-      borderColor: "rgba(237, 155, 40)",
-      borderWidth: 1,
-      data: [65, 65, 65, 59, 59, 59, 80, 80, 80, 160, 120, 160],
-    },
-  ],
-};
-
-export default class LineChart extends React.Component {
-  render() {
-    return (
-      <div>
-        <Line
-          data={state}
-          options={{
-            elements: {
-              Line: {
-                borderJoinStyle: "round",
-                tension: 0.5,
-              },
-              title: {
-                display: true,
-                text: "Average Rainfall per month",
-                fontSize: 20,
-              },
-              legend: {
-                display: true,
-                position: "right",
-              },
-            },
-          }}
-        />
-      </div>
-    );
+function LineChart({graphOneReadings}){
+  const graphLabels = []
+  
+  
+  const state = {
+    labels: [90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200],
+    datasets: [
+      {
+        label: "Mmol/l",
+        backgroundColor: "rgba(237, 155, 40)",
+        borderColor: "rgba(237, 155, 40)",
+        borderWidth: 1,
+        data: [65, 65, 65, 59, 59, 59, 80, 80, 80, 160, 120, 160],
+      },
+    ],
   }
+  return (
+    <div>
+      <Line
+        data={state}
+        options={{
+          elements: {
+            Line: {
+              borderJoinStyle: "round",
+              tension: 0.5,
+            },
+            title: {
+              display: true,
+              text: "Average Rainfall per month",
+              fontSize: 20,
+            },
+            legend: {
+              display: true,
+              position: "right",
+            },
+          },
+        }}
+      />
+    </div>
+  );
 }
+
+
+    
+
+
+export default LineChart
--- src/views/profile/Profile.js
+++ src/views/profile/Profile.js
@@ -1,14 +1,23 @@
 import LineChart from "../../components/LineChart";
 import "./styles.css";
 import Title from "../../components/Title";
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 
 function Profile(){
-  const [readings, setReadings] = useState([])
+  const [allReadings, setAllReadings] = useState([])
+  
 
-  fetch('/api/v1/readings')
-  .then(res => res.json())
-  .then(data => console.log(data))
+  useEffect(()=>{
+    fetch('/api/v1/readings')
+    .then(res => res.json())
+    .then(data => setAllReadings(data))
+  }, [])
+  
+  const graphOneReadings = [allReadings[0], allReadings[1], allReadings[2], allReadings[3], allReadings[4]]
+  
+  const graphTwoReadings = [allReadings[5], allReadings[6], allReadings[7], allReadings[8], allReadings[9]]
+
+  const graphThreeReadings = [allReadings[10], allReadings[11], allReadings[12], allReadings[13], allReadings[14]]
 
 
   return (
@@ -18,7 +27,7 @@ function Profile(){
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.js"></script>
         <div className="card chart-container">
           Year to Date Measurements
-          <LineChart />
+          <LineChart graphOneReadings={graphOneReadings} />
           <br/>
           Monthly Measurements
           <LineChart />
