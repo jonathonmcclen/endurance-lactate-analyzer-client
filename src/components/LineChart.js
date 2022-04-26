5f30db1c1f527aec8178407fe929184916f5a028
AlGuise <98363807+AlGuise@users.noreply.github.com>
2022-04-26 10:39:02 -0600

    Update LineChart.js

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -13,8 +13,8 @@ function LineChart() {
     datasets: [
       {
         label: "Mmol/l",
-        backgroundColor: "rgba(237, 155, 40)",
-        borderColor: "rgba(237, 155, 40)",
+        backgroundColor: "rgba(24, 66, 120)",
+        borderColor: "rgba(241, 241, 241)",
         borderWidth: 1,
         data: [...graphOneReadings.mmol],
       },
