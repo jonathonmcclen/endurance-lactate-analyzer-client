d69d90528fddc3007fd7791283900747902b1630
--replace-all <jonathonsmcclendon@gmail.com>
2022-04-28 15:12:54 -0500

    Update Home.js

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -7,30 +7,40 @@ const Home = () => {
   return (
     <>
       <section className="light">
-        <Title text="" width="100%" />
         <div>
           <img src={StepAndRepeat} width="100%" />
         </div>
 
-        <section style={{ 
-          padding: "40px"
-          }}>
+        <section
+          style={{
+            padding: "40px",
+          }}
+        >
           <div className="row">
-
-            <div id='enduracne-about-text-container'>
+            <div id="enduracne-about-text-container">
               <h1>Endurance Lactate Analyzer</h1>
 
               <p>
-              ENDURANCE is the next step in biometric feedback for endurance athletes. Using data gathered from blood lactate testing, our system enables athletes to view real-time status on their lactate levels. Easily track, store, and share data with coaches, physicians, and physical therapists, and push the boundaries of your performance with ENDURANCE.
+                ENDURANCE is the next step in biometric feedback for endurance
+                athletes. Using data gathered from blood lactate testing, our
+                system enables athletes to view real-time status on their
+                lactate levels. Easily track, store, and share data with
+                coaches, physicians, and physical therapists, and push the
+                boundaries of your performance with ENDURANCE.
               </p>
               <div className="row">
                 <div className="col-4"> </div>
                 <div className="col-4">
-                  <a href="/registration" className="btn btn-lg btn-orchird" style={{
-                    backgroundColor: '#3b76c4', 
-                    color:"white", 
-                    position:'relative',
-                    right: '20px'}}>
+                  <a
+                    href="/registration"
+                    className="btn btn-lg btn-orchird"
+                    style={{
+                      backgroundColor: "#3b76c4",
+                      color: "white",
+                      position: "relative",
+                      right: "20px",
+                    }}
+                  >
                     Join
                   </a>
                 </div>
